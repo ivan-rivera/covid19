93d693951a8524ab88eb12ff2cd3bad317840714
ivan.s.rivera@gmail.com <ivan.s.rivera@gmail.com>
2020-03-25 15:33:38 +0100

    testing black

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from decouple import config
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     return "Hello World 3!"
 
 
-if __name__ == '__main__':
-    PORT = config(
-        "PORT", default=8000,
-        cast=int)
-    app.run(host="0.0.0.0" , port = PORT)
+if __name__ == "__main__":
+    PORT = config("PORT", default=8000, cast=int)
+    app.run(host="0.0.0.0", port=PORT)
